e959428dcca042946831fdc32e42d071ba2b0245
Samuel Odira <52766763+czsamzzy@users.noreply.github.com>
2020-08-01 14:53:11 +0100

    full script done

--- Js/script.js
+++ Js/script.js
@@ -1,15 +1,17 @@
 //DOM Elements
 const time = document.getElementById("time");
-greeting = document.getElementById("greeting");
-name = document.getElementById("name");
-focus = document.getElementById("focus");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const focus = document.getElementById("focus");
+
+const showAmPm = true;
 
 // Show Time
 function showTime() {
   let today = new Date();
-  hour = today.getHours();
-  min = today.getMinutes();
-  sec = today.getSeconds();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
 
   // Set AM or PM
 
@@ -19,11 +21,92 @@ function showTime() {
   hour = hour % 12 || 12;
 
   // Output Time
-  time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${showAmPm ? amPm : ""}`;
 
   setTimeout(showTime, 1000);
 }
 
+// Add Zeros
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// SET BAckground and Greeting
+function setBgGreet() {
+  let today = new Date();
+  let hour = today.getHours();
+
+  if (hour < 12) {
+    // Morning
+    document.body.style.backgroundColor = "green";
+    greeting.textContent = "Good Morning";
+  } else if (hour < 18) {
+    // Afternoon
+    document.body.style.backgroundColor = "blue";
+    greeting.textContent = "Good Afternoon";
+  } else {
+    // Evening
+    document.body.style.backgroundColor = "black";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
+  }
+}
+
+// Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set name
+function setName(e) {
+  if (e.type === "keypress") {
+    // make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.InnerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.InnerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+// Set Focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.InnerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.InnerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
 // Run
 
 showTime();
+setBgGreet();
+getName();
+getFocus();
